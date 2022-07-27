5757a20a8d2e838f2712e6c93b3d8174e2e9a26b
Vitaliy Vaskivskiy <ipz204_vvyu@student.ztu.edu.ua>
2022-07-27 19:37:10 +0300

    added calendar days service

--- TimeTracker.Server/Services/CalendarDaysService.cs
+++ TimeTracker.Server/Services/CalendarDaysService.cs
@@ -5,16 +5,32 @@ namespace TimeTracker.Server.Services
     public class CalendarDaysService
     {
         private readonly ICalendarDayManager calendarDayManager;
+        private readonly ISettingsManager settingsManager;
 
-        public CalendarDaysService(ICalendarDayManager calendarDayManager)
+        public CalendarDaysService(ICalendarDayManager calendarDayManager, ISettingsManager settingsManager)
         {
             this.calendarDayManager = calendarDayManager;
+            this.settingsManager = settingsManager;
         }
 
         public async Task<int> GetAmountWorkingHoursInMonth(DateTime dateTime)
         {
-
-            return 0;
+            var from = new DateTime(dateTime.Year, dateTime.Month, 1);
+            var to = from.AddMonths(1).AddDays(-1);
+            var calendarDaysInMonth =  await calendarDayManager.GetAsync(from, to);
+            var settings = await settingsManager.GetAsync();
+            var givenMonthDays = Enumerable.Range(0, 1 + to.Subtract(from).Days)
+                .Select(offset => from.AddDays(offset))
+                .ToArray();
+            return givenMonthDays.Sum(day =>
+            {
+                var calendarDayInMonth = calendarDaysInMonth.FirstOrDefault(calendarDay => calendarDay.Date == day);
+                if (calendarDayInMonth == null)
+                {
+                    return settings.Employment.FullTimeHoursInWorkday;
+                }
+                return settings.Employment.FullTimeHoursInWorkday * calendarDayInMonth.PercentageWorkHours / 100;
+            });
         }
     }
 }
